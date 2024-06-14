55d0812194ed772dad2aad02899ee3b413f6b165
V11an <allankertich@gmail.com>
2024-06-14 20:17:03 +0300

    about page

--- app.py
+++ app.py
@@ -178,6 +178,10 @@ def logout():
     session.clear()
     return redirect("/")
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('index.html')
